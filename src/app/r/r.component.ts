49a4680741663bfebdf99f4895815940cfc45d52
sdoumaima <saidioumaima21@gmail.com>
2023-05-14 12:47:00 +0100

    first commit

---
+++ src/app/r/r.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-r',
+  templateUrl: './r.component.html',
+  styleUrls: ['./r.component.css']
+})
+export class RComponent {
+  showNotifications: boolean = false;
+  
+  toggleNotifications() {
+    this.showNotifications = !this.showNotifications;
+   }
+   // ...
+  exp: any = [];
+  formation: any = [];
+  langue: any = [];
+  university: any = [];
+
+
+  addlangue() {
+    this.langue.push(
+      {
+        langue: "",
+        niveau: ""
+      }
+    )
+  }
+
+  addexp(){
+    this.exp.push(
+      {
+        entreprise: "",
+        city: "",
+        fonction:"",
+        debutmois: "",
+        debutannee: "",
+        finmois: "",
+        finannee: "",
+        details: ""
+      }
+    )
+  }
+
+  adduniversity(){
+    this.university.push(
+      {
+        fac: "",
+        diploma: "",
+        debutmois: "",
+        debutannee: "",
+
+      }
+    )
+  }
+
+  addformation(){
+    this.formation.push(
+      {
+        centre: "",
+        diploma: "",
+        debutmois: "",
+        debutannee: "",
+
+      }
+    )
+  }
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    console.log(file);
+    // Vous pouvez envoyer la vidéo au serveur à partir d'ici
+  }
+
+}
